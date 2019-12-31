91d0cd4078c32653461f58e22451e357077c9a2d
lewys <lewysward99@gmail.com>
2019-12-31 00:28:51 +0000

    Added function signatures to threadMap.h

--- ThreadMap/threadMap.h
+++ ThreadMap/threadMap.h
@@ -7,10 +7,17 @@
 //TODO - add each function required by scheduler
 //TODO - Make each function thread safe using std::lock_guard and std::mutex
 
-
 class ThreadMap {
 public:
+    std::map<uint16_t, struct thread*> data;
     ThreadMap() {std::cout << "Creating new ThreadMap!" << std::endl;}
+    struct thread* getCurrentThread();
+    struct thread* getThread(unint16_t id);
+    void add(struct thread* thread);
+    void delete(struct thread* thread);
+    void print();
+    bool isEmpty();
+
 };
 
 
